ce1d21e337ec26631c319fc7cdf41ea10e77b505
johy119 <zyiming1995@gmail.com>
2020-01-06 08:57:17 +0800

    数据展示页 1/2

--- mock/table.js
+++ mock/table.js
@@ -1,21 +1,55 @@
 import Mock from 'mockjs'
 
-const data = Mock.mock({
-  'items|30': [{
-    id: '@id',
-    title: '@sentence(10, 20)',
-    'status|1': ['published', 'draft', 'deleted'],
-    author: 'name',
-    display_time: '@datetime',
-    pageviews: '@integer(300, 5000)'
-  }]
-})
+// const data = Mock.mock({
+//   'items|30': [{
+//     'id|+1': 1,
+//     title: '@sentence(10, 20)',
+//     'status|1': ['published', 'draft', 'deleted'],
+//     author: 'name',
+//     display_time: '@datetime',
+//     pageviews: '@integer(300, 5000)'
+//   }]
+// })
+// const data = Mock.mock({
+//   'items|30': [{
+//     'id|+1': 1,
+//     days: '@integer(300, 5000)',
+//     temperature: '@integer(300, 5000)',
+//     moisture: '@integer(300, 5000)',
+//     pressure: '@integer(300, 5000)',
+//     num_fans: '@integer(300, 5000)',
+//     angle_window: '@integer(300, 5000)',
+//     angle_door: '@integer(300, 5000)',
+//     is_pump: '@boolean',
+//     is_heating: '@boolean',
+//     is_light: '@boolean',
+//     numbers: '@integer(300, 5000)',
+//     update_time: '@datetime'
+//   }]
+// })
 
 export default [
   {
     url: '/table/list',
     type: 'get',
     response: config => {
+      const data = Mock.mock({
+        'items|30': [{
+          'id|+1': 1,
+          days: '@integer(300, 5000)',
+          temperature: '@integer(300, 5000)',
+          moisture: '@integer(300, 5000)',
+          pressure: '@integer(300, 5000)',
+          num_fans: '@integer(300, 5000)',
+          angle_window: '@integer(300, 5000)',
+          angle_door: '@integer(300, 5000)',
+          is_pump: '@boolean',
+          is_heating: '@boolean',
+          is_light: '@boolean',
+          numbers: '@integer(300, 5000)',
+          update_time: '@datetime'
+        }]
+      })
       const items = data.items
       return {
         code: 20000,
--- src/views/table/index.vue
+++ src/views/table/index.vue
@@ -2,42 +2,103 @@
   <div class="app-container">
     <el-table
       v-loading="listLoading"
+      stripe
       :data="list"
       element-loading-text="Loading"
       border
       fit
-      highlight-current-row
+      @cell-mouse-enter.stop
     >
-      <el-table-column align="center" label="ID" width="95">
+      <el-table-column key="id" align="center" label="舍号">
         <template slot-scope="scope">
-          {{ scope.$index }}
+          {{ scope.row.id }}
         </template>
       </el-table-column>
-      <el-table-column label="Title">
+      <el-table-column key="days" align="center" label="日龄">
         <template slot-scope="scope">
-          {{ scope.row.title }}
+          {{ scope.row.days }}
         </template>
       </el-table-column>
-      <el-table-column label="Author" width="110" align="center">
+      <el-table-column key="temperature" label="实测温度" align="center">
         <template slot-scope="scope">
-          <span>{{ scope.row.author }}</span>
+          <span>{{ scope.row.temperature }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="Pageviews" width="110" align="center">
+      <el-table-column key="moisture" label="实测湿度" width="110" align="center">
         <template slot-scope="scope">
-          {{ scope.row.pageviews }}
+          {{ scope.row['moisture'] }}
         </template>
       </el-table-column>
-      <el-table-column class-name="status-col" label="Status" width="110" align="center">
+      <el-table-column key="pressure" label="实测负压" align="center">
         <template slot-scope="scope">
-          <el-tag :type="scope.row.status | statusFilter">{{ scope.row.status }}</el-tag>
+          <span>{{ scope.row.pressure }}</span>
         </template>
       </el-table-column>
-      <el-table-column align="center" prop="created_at" label="Display_time" width="200">
+      <el-table-column key="num_fans" label="风机数量" align="center">
+        <template slot-scope="scope">
+          <span>{{ scope.row.num_fans }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column key="angle_window" label="小窗角度" align="center">
+        <template slot-scope="scope">
+          <span>{{ scope.row.angle_window }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column key="angle_door" label="保温门角度" align="center">
+        <template slot-scope="scope">
+          <span>{{ scope.row.angle_door }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column key="is_pump" label="水泵" align="center">
+        <template slot-scope="scope">
+          <el-tag :type="scope.row.is_pump?'success':'danger'">{{ scope.row.is_pump?'开':'关' }}</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column key="is_heating" label="加热" align="center">
+        <template slot-scope="scope">
+          <el-tag :type="scope.row.is_heating?'success':'danger'">{{ scope.row.is_pump?'开':'关' }}</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column key="is_light" label="灯光" align="center">
+        <template slot-scope="scope">
+          <el-tag :type="scope.row.is_light?'success':'danger'">{{ scope.row.is_pump?'开':'关' }}</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column key="numbers" label="存栏量" align="center">
+        <template slot-scope="scope">
+          <span>{{ scope.row.numbers }}</span>
+        </template>
+      </el-table-column>
+<!--      <el-table-column class-name="status-col" label="Status" width="110" align="center">-->
+<!--        <template slot-scope="scope">-->
+<!--          <el-tag :type="scope.row.status | statusFilter">{{ scope.row.status }}</el-tag>-->
+<!--        </template>-->
+<!--      </el-table-column>-->
+      <el-table-column align="center" label="更新时间" width="200">
         <template slot-scope="scope">
           <i class="el-icon-time" />
-          <span>{{ scope.row.display_time }}</span>
+          <span>{{ scope.row.update_time }}</span>
         </template>
+      </el-table-column>
+
+      <el-table-column label="操作项" align="center" width="230" >
+        <el-popover
+          ref="action-popover"
+          open-delay="100"
+          close-delay="100"
+          placement="bottom"
+          trigger="hover"
+          >
+          <div align="center">
+            <el-button-group align="center">
+<!--              <el-button type="info">修改</el-button>-->
+              <el-button type="primary">查看详情</el-button>
+<!--              <el-button type="danger">删除</el-button>-->
+            </el-button-group>
+
+          </div>
+        </el-popover>
+        <el-button v-popover:action-popover>操作</el-button>
       </el-table-column>
     </el-table>
   </div>
@@ -60,15 +121,22 @@ export default {
   data() {
     return {
       list: null,
-      listLoading: true
+      listLoading: true,
+      timer: 0
     }
   },
   created() {
     this.fetchData()
   },
+  mounted() {
+    this.timer = setInterval(this.fetchData, 1000)
+  },
+  beforeDestroy() {
+    clearTimeout(this.timer)
+  },
   methods: {
     fetchData() {
-      this.listLoading = true
+      // this.listLoading = true
       getList().then(response => {
         this.list = response.data.items
         this.listLoading = false
